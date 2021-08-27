b609a224f18de4495fba9a8d751b650a458a661b
pclaudio3 <paolo.claudio3@gmail.com>
2021-08-27 12:53:05 -0400

    feat: add client request and response types to BaseClient

--- synapse-client-rest/src/main/java/io/americanexpress/synapse/client/rest/client/BaseClient.java
+++ synapse-client-rest/src/main/java/io/americanexpress/synapse/client/rest/client/BaseClient.java
@@ -13,6 +13,8 @@
  */
 package io.americanexpress.synapse.client.rest.client;
 
+import java.lang.reflect.ParameterizedType;
+
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 import org.springframework.http.HttpMethod;
@@ -36,10 +38,20 @@ abstract class BaseClient<I extends BaseClientRequest, O extends BaseClientRespo
      */
     protected final XLogger logger = XLoggerFactory.getXLogger(getClass());
 	
+    /**
+     * Client request type which is determined from the generic type argument <I>.
+     */
+    protected Class<I> clientRequestType;
+    
+    /**
+     * Client response type which is determined from the generic type argument <O>.
+     */
+    protected Class<O> clientResponseType;
+    
 	/**
      * HTTP headers factory used to produce the custom HTTP headers required to consume the back end service.
      */
-    protected H httpHeadersFactory;
+    protected final H httpHeadersFactory;
 	
 	/**
      * HTTP method of the back end service.
@@ -50,7 +62,7 @@ abstract class BaseClient<I extends BaseClientRequest, O extends BaseClientRespo
      * URL of the back end service injected from the application properties.
      */
     protected String url;
-
+    
     /**
      * Argument constructor creates a new instance of BaseClient with given values.
      * @param httpHeadersFactory HTTP headers factory used to produce the custom HTTP headers required to consume the back end service
@@ -59,6 +71,7 @@ abstract class BaseClient<I extends BaseClientRequest, O extends BaseClientRespo
     protected BaseClient(H httpHeadersFactory, HttpMethod httpMethod) {
     	this.httpHeadersFactory = httpHeadersFactory;
     	this.httpMethod = httpMethod;
+    	initialize();
     }
     
     /**
@@ -95,5 +108,27 @@ abstract class BaseClient<I extends BaseClientRequest, O extends BaseClientRespo
      */
     public void setUrl(String url) {
         this.url = url;
+    }
+    
+    /**
+     * Initialize this client.
+     */
+    @SuppressWarnings("unchecked")
+    private void initialize() {
+    	
+    	// Find the first child class of BaseClient
+    	// in order to determine the request and response types
+    	Class<?> classType = getClass();
+    	while(classType.getSuperclass() != BaseClient.class) {
+    		classType = classType.getSuperclass();
+    	}
+    	
+    	// Set the client request and response types
+    	// which were found from the generic type arguments of BaseClient<0 = I, 1 = O, 2 = H>
+    	// Here, the client request type is from the 0th generic type argument <I>
+    	// and the client response type is from the 1st generic type argument <O>
+    	ParameterizedType parameterizedType = ((ParameterizedType) getClass().getGenericSuperclass());
+    	clientRequestType = (Class<I>) parameterizedType.getActualTypeArguments()[0];
+    	clientResponseType = (Class<O>) parameterizedType.getActualTypeArguments()[1];
     }
 }
