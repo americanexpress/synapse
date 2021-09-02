6980325e5fbf600876e6e18703dfcb581a0592bb
amorgad <alexei.morgado@aexp.com>
2021-09-02 11:29:31 -0400

    Made synapse work internally, only warnings for unused libraries left.

---
+++ synapse-data-couchbase/src/main/java/io/americanexpress/synapse/data/couchbase/config/BaseCrudCouchbaseDataConfig.java
@@ -0,0 +1,19 @@
+package io.americanexpress.synapse.data.couchbase.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.env.Environment;
+import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
+
+/**
+ * The type Base couchbase data config.
+ *
+ * @author dliburd
+ */
+@EnableCouchbaseRepositories
+public abstract class BaseCrudCouchbaseDataConfig extends io.americanexpress.synapse.data.couchbase.config.BaseCouchbaseDataConfig {
+
+	protected BaseCrudCouchbaseDataConfig(final Environment environment, final ObjectMapper objectMapper) {
+		super(environment, objectMapper);
+	}
+
+}
---
+++ synapse-data-couchbase/src/main/java/io/americanexpress/synapse/data/couchbase/repository/BaseCrudCouchbaseRepository.java
@@ -0,0 +1,12 @@
+package io.americanexpress.synapse.data.couchbase.repository;
+
+import io.americanexpress.synapse.data.couchbase.entity.BaseCouchbaseEntity;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * @author dliburd
+ */
+@NoRepositoryBean
+public interface BaseCrudCouchbaseRepository<T extends BaseCouchbaseEntity, I> extends PagingAndSortingRepository<T, I> {
+}
