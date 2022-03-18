b0f28b420def9b08de1bfed4bced5158aee34805
pclaudio3 <paolo.claudio3@gmail.com>
2022-03-18 11:32:12 -0400

    feat: indentation

--- archetype/synapse-archetype-client-rest-reactive-post/src/main/resources/archetype-resources/src/main/java/client/__className__PostReactiveRestClient.java
+++ archetype/synapse-archetype-client-rest-reactive-post/src/main/resources/archetype-resources/src/main/java/client/__className__PostReactiveRestClient.java
@@ -33,10 +33,10 @@ import ${package}.model.${className}ClientResponse;
 public class ${className}PostReactiveRestClient extends BasePostReactiveRestClient<${className}ClientRequest, ${className}ClientResponse, ${className}ClientHttpHeadersFactory> {
 
 	/**
-     * Argument constructor creates a new instance of ${className}PostReactiveRestClient with given values.
-     * @param clientHttpHeadersFactory HTTP headers factory used to produce the custom HTTP headers required to consume the back end service
-     * @param reactiveRestResponseErrorHandler used to handle errors from the reactive REST client
-     */
+	 * Argument constructor creates a new instance of ${className}PostReactiveRestClient with given values.
+	 * @param clientHttpHeadersFactory HTTP headers factory used to produce the custom HTTP headers required to consume the back end service
+	 * @param reactiveRestResponseErrorHandler used to handle errors from the reactive REST client
+	 */
 	@Autowired
 	public ${className}PostReactiveRestClient(${className}ClientHttpHeadersFactory clientHttpHeadersFactory, ${className}ReactiveRestResponseErrorHandler reactiveRestResponseErrorHandler) {
 		super(clientHttpHeadersFactory, reactiveRestResponseErrorHandler);
