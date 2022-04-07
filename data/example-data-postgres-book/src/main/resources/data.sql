342537239dc7943a7ef6b34467d71479453a4697
amorgado <alexei_morgado@yahoo.com>
2022-04-07 12:06:26 -0400

    Removed my personal email address and public book names also for fake ones.

--- data/example-data-postgres-book/src/main/resources/data.sql
+++ data/example-data-postgres-book/src/main/resources/data.sql
@@ -2,9 +2,9 @@ SET
 SCHEMA_SEARCH_PATH TO synapse;
 insert into book (id, title, author, created_date, last_modified_date, created_by,
                     last_modified_by, version)
-values (1, 'White Fang', 'Jack London', now(),
-        now(), 'alexei_morgado@yahoo.com', 'alexei_morgado@yahoo.com', 0);
+values (1, 'Synapse', 'Gabriel', now(),
+        now(), 'John-Appleseed@email.com', 'John-Appleseed@email.com', 0);
 insert into book (id, title, author, created_date, last_modified_date, created_by,
                     last_modified_by, version)
-values (2, 'The Lord of the Rings', 'J. R. R. Tolkien', now(),
-        now(), 'alexei_morgado@yahoo.com', 'alexei_morgado@yahoo.com', 0);
+values (2, 'Revenge of Synapse', 'John', now(),
+        now(), 'John-Appleseed@email.com', 'John-Appleseed@email.com', 0);
