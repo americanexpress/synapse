0387501f796233684df77f85ad1882063050c410
Wendy Hu <Wendy.Hu1@aexp.com>
2022-05-26 17:46:10 -0400

    bugfix(): fixing rest client unit test to throw ApplicationClientException

--- client/synapse-client-test/src/main/java/io/americanexpress/synapse/client/test/client/BaseRestClientUnitTest.java
+++ client/synapse-client-test/src/main/java/io/americanexpress/synapse/client/test/client/BaseRestClientUnitTest.java
@@ -156,7 +156,7 @@ public abstract class BaseRestClientUnitTest<I extends BaseClientRequest,
     @Test
     void callMonoService_givenServerError_expectedApplicationClientException() throws IOException {
         responseActions.andRespond(withStatus(HttpStatus.INTERNAL_SERVER_ERROR).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsString(clientResponse)));
-        Assertions.assertThrows(ApplicationServerException.class, this::callMonoServiceAndAssertErrorResponse, CommonAssertionMessages.EXCEPTION_NOT_THROWN);
+        Assertions.assertThrows(ApplicationClientException.class, this::callMonoServiceAndAssertErrorResponse, CommonAssertionMessages.EXCEPTION_NOT_THROWN);
     }
     
     private MockRestServiceServer getMockServer() {
