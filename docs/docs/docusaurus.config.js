6d2c1e5a2399f0d458884f711145adcda640be6b
whoswendy <wendy.hu@alumni.stonybrook.edu>
2022-06-19 03:49:35 -0400

    feat(docusaurus-docs): adding docusaurus website (#95)
    * feat(docusaurus-docs): adding docusaurus website
    
    * fix(): removing unneeded files
    
    * fix(): remove aexp url

---
+++ docs/docs/docusaurus.config.js
@@ -0,0 +1,99 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Synapse',
+  tagline: 'The future of development is here',
+  url: 'https://americanexpress.io',
+  baseUrl: '/synapse/docs/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/logo.png',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'americanexpress', // Usually your GitHub org/user name.
+  projectName: 'synapse',
+  trailingSlash: false,// Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: 'real_docs',
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Synapse',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.png',
+          href: 'https://americanexpress.io/synapse/',
+          target: '_self',
+        },
+      },
+      footer: {
+        style: 'dark',
+        logo: {
+          alt: 'Synapse',
+          src: 'img/logo.png',
+        },
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/synapse',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/americanexpress/synapse',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Synapse`,
+      },
+      colorMode: {
+        defaultMode: 'dark'
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ docs/docs/real_docs/client/synapse-client-test.md
@@ -0,0 +1,21 @@
+# synapse-client-test
+
+## Description
+
+- This is the synapse module that provides the base classes to test the spring restful clients. It provides several
+  out-of-the-box functionalities like:
+
+    - An open to extension BaseRestClientUnitTest class which already calls several methods to unit test all the
+      scenarios in the concrete client unit test class that extend this.
+    - An open to extension BaseRestClientUnitIT class that will require only the concrete rest client IT class
+      implementing it to override one success method which will be the integration test method needed.
+
+## Usage
+- To utilize this module, add the following dependency to the pom.xml file:
+```
+        <dependency>
+            <groupId>com.americanexpress</groupId>
+            <artifactId>synapse-client-test</artifactId>
+            <version>1.0.0-SNAPSHOT</version>
+        </dependency>
+```
