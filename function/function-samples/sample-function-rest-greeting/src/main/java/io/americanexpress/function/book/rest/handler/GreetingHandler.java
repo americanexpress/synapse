cf2ba74e2a6feb7ef12754e31ae722a37c3d5b88
Gabriel Jimenez <gabriel.a.jimenez@aexp.com>
2022-10-04 09:25:19 -0400

    feat(cloud function): adding new function sample

---
+++ function/function-samples/sample-function-rest-greeting/src/main/java/io/americanexpress/function/book/rest/handler/GreetingHandler.java
@@ -0,0 +1,19 @@
+package io.americanexpress.function.book.rest.handler;
+
+import io.americanexpress.function.book.rest.model.Greeting;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class GreetingHandler {
+
+  public Mono<ServerResponse> hello(ServerRequest request) {
+    return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+      .body(BodyInserters.fromValue(new Greeting("Hello, Spring!")));
+  }
+}
