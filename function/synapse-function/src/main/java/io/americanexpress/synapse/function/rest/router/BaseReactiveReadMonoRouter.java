b384097732dac933f9464e38496b6b8dca7fc78e
Gabriel Jimenez <gabriel.a.jimenez@aexp.com>
2022-10-05 17:09:05 -0400

    feat(cloud function): fixing module name change - synapse-function

--- function/synapse-function/src/main/java/io/americanexpress/synapse/function/rest/router/BaseReactiveReadMonoRouter.java
+++ function/synapse-function/src/main/java/io/americanexpress/synapse/function/rest/router/BaseReactiveReadMonoRouter.java
@@ -24,6 +24,7 @@ import javax.validation.Valid;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
