7fe661d18110bffddb8659cfa65d5239df94854c
ffgutt <guttff@gmail.com>
2022-10-11 08:46:59 -0400

    fix(reactive): greeting example get

--- function/function-samples/sample-function-greeting/src/main/java/io/americanexpress/function/greeting/reactive/router/GetMonoGreetingRouter.java
+++ function/function-samples/sample-function-greeting/src/main/java/io/americanexpress/function/greeting/reactive/router/GetMonoGreetingRouter.java
@@ -1,9 +1,7 @@
 package io.americanexpress.function.greeting.reactive.router;
 
 import io.americanexpress.function.greeting.reactive.handler.GetMonoGreetingHandler;
-import io.americanexpress.function.greeting.reactive.handler.ReadMonoGreetingHandler;
 import io.americanexpress.synapse.function.reactive.router.BaseGetMonoRouter;
-import io.americanexpress.synapse.function.reactive.router.BaseReadMonoRouter;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
