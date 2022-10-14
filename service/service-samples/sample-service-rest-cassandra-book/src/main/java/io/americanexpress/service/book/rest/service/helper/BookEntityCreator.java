aa659b8f5d1cb8bc28806a5829b8ebf8eada3649
Wendy Hu <wendy.hu@alumni.stonybrook.edu>
2022-10-14 16:46:32 -0400

    fix(): removing identifier

--- service/service-samples/sample-service-rest-cassandra-book/src/main/java/io/americanexpress/service/book/rest/service/helper/BookEntityCreator.java
+++ service/service-samples/sample-service-rest-cassandra-book/src/main/java/io/americanexpress/service/book/rest/service/helper/BookEntityCreator.java
@@ -32,7 +32,6 @@ public class BookEntityCreator {
      */
     public static BookEntity create(String title, String author, int numberOfCopies) {
         BookEntity book = new BookEntity(title, author);
-        book.setIdentifier(UUID.randomUUID());
         book.setNumberOfCopies(numberOfCopies);
         return book;
     }
