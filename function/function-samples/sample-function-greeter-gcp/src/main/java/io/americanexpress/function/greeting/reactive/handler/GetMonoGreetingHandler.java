5515e1663e7a235baff023d2e673f491580b6215
Gabriel Jimenez <gabriel.a.jimenez@aexp.com>
2022-10-20 23:26:02 -0400

    feat(gcp function): gcp function sample greeter

---
+++ function/function-samples/sample-function-greeter-gcp/src/main/java/io/americanexpress/function/greeting/reactive/handler/GetMonoGreetingHandler.java
@@ -0,0 +1,15 @@
+//package io.americanexpress.function.greeting.reactive.handler;
+//
+//import io.americanexpress.function.greeting.reactive.model.Greeting;
+//import io.americanexpress.synapse.function.reactive.handler.BaseGetMonoHandler;
+//import io.americanexpress.synapse.function.reactive.handler.BaseReadMonoHandler;
+//import org.springframework.http.MediaType;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.reactive.function.BodyInserters;
+//import org.springframework.web.reactive.function.server.ServerRequest;
+//import org.springframework.web.reactive.function.server.ServerResponse;
+//import reactor.core.publisher.Mono;
+//
+//public class MyStringHandlers extends SpringBootRequestHandler<String, String> {
+//
+//}
