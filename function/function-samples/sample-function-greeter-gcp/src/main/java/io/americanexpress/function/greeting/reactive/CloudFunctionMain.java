63a06c497ca1940989422e51f178c813615a2f50
Gabriel Jimenez <gabriel.a.jimenez@aexp.com>
2022-10-20 23:26:38 -0400

    feat(gcp function): gcp function sample greeter

---
+++ function/function-samples/sample-function-greeter-gcp/src/main/java/io/americanexpress/function/greeting/reactive/CloudFunctionMain.java
@@ -0,0 +1,20 @@
+package io.americanexpress.function.greeting.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class CloudFunctionMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFunctionMain.class, args);
+    }
+
+    @Bean
+    public Function<String, String> uppercase() {
+        return value -> value.toUpperCase();
+    }
+}
