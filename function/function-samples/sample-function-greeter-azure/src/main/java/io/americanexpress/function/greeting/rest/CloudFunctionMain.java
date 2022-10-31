6026446f3011f6740ebdbf9a8d61070da66e8588
Wendy Hu <wendy.hu@alumni.stonybrook.edu>
2022-10-31 13:53:08 -0400

    feat(sample-function-azure): init azure sample

---
+++ function/function-samples/sample-function-greeter-azure/src/main/java/io/americanexpress/function/greeting/rest/CloudFunctionMain.java
@@ -0,0 +1,13 @@
+package io.americanexpress.function.greeting.rest;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFunctionMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFunctionMain.class, args);
+    }
+}
