86cba961b93ae54ddcc7483a1cc223d460d14f47
Wendy Hu <wendy.hu@alumni.stonybrook.edu>
2022-11-01 09:06:52 -0400

    docs(): adding copyright

--- function/function-samples/sample-function-greeter-aws/src/main/java/io/americanexpress/function/greeting/rest/function/Greet.java
+++ function/function-samples/sample-function-greeter-aws/src/main/java/io/americanexpress/function/greeting/rest/function/Greet.java
@@ -1,13 +1,24 @@
+/*
+ * Copyright 2020 American Express Travel Related Services Company, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package io.americanexpress.function.greeting.rest.function;
 
 import io.americanexpress.function.greeting.rest.model.Greeting;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.function.Function;
