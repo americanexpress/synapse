f19c0913456345b09dd2587ca7778e6677f4a961
Wendy Hu <wendy.hu@alumni.stonybrook.edu>
2022-11-02 11:45:05 -0400

    feat(sample-function-book-aws): adding crud operations

--- function/function-samples/sample-function-book-aws/src/main/java/io/americanexpress/function/book/function/BookFunction.java
+++ function/function-samples/sample-function-book-aws/src/main/java/io/americanexpress/function/book/function/BookFunction.java
@@ -1,14 +1,26 @@
 package io.americanexpress.function.book.function;
 
+import io.americanexpress.data.book.entity.BookEntity;
 import io.americanexpress.data.book.repository.BookRepository;
 import io.americanexpress.function.book.function.helper.BookEntityCreator;
+import io.americanexpress.function.book.function.helper.ReadBookResponseCreator;
+import io.americanexpress.function.book.model.BookRequest;
 import io.americanexpress.function.book.model.CreateBookRequest;
 import io.americanexpress.function.book.model.CreateBookResponse;
+import io.americanexpress.function.book.model.ReadBookRequest;
+import io.americanexpress.function.book.model.ReadBookResponse;
+import io.americanexpress.function.book.model.UpdateBookRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.messaging.Message;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @Configuration
 public class BookFunction {
@@ -20,8 +32,45 @@ public class BookFunction {
     }
 
     @Bean
-    public Function<CreateBookRequest, Mono<CreateBookResponse>> create() {
-        return request -> bookRepository.save(BookEntityCreator.create(request.getTitle(), request.getAuthor(), 1))
+    public Function<Message<CreateBookRequest>, Mono<CreateBookResponse>> create() {
+        return request -> bookRepository.save(BookEntityCreator.create(request.getPayload().getTitle(), request.getPayload().getAuthor(), 1))
                     .map(bookEntity -> new CreateBookResponse());
     }
+
+    @Bean
+    public Consumer<Message<CreateBookRequest>> createBook() {
+        return request -> bookRepository.save(BookEntityCreator.create(request.getPayload().getTitle(), request.getPayload().getAuthor(), 1));
+    }
+
+    @Bean
+    public Function<Message<ReadBookRequest>, Mono<ReadBookResponse>> read() {
+        return request -> bookRepository.findByTitleAndAuthor(request.getPayload().getTitle(), request.getPayload().getAuthor())
+                .map(ReadBookResponseCreator::create);
+    }
+
+    @Bean
+    public Supplier<Flux<ReadBookResponse>> get() {
+        return () -> bookRepository.findAll().map(ReadBookResponseCreator::create);
+    }
+
+    @Bean
+    public Consumer<Message<UpdateBookRequest>> update() {
+        return request -> bookRepository.findByTitleAndAuthor(request.getPayload().getTitle(), request.getPayload().getAuthor())
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Book Not Found")))
+                .map(book -> updateBook(book, request.getPayload().getNumberOfCopies()))
+                .flatMap(bookRepository::save);
+    }
+
+    private BookEntity updateBook(BookEntity book, int numOfCopies){
+        if (book != null) book.setNumberOfCopies(numOfCopies);
+        return book;
+    }
+
+    @Bean
+    public Consumer<Message<BookRequest>> delete() {
+        return request -> bookRepository.findByTitleAndAuthor(request.getPayload().getTitle(), request.getPayload().getAuthor())
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Book Not Found")))
+                .flatMap(bookRepository::delete);
+    }
+
 }
