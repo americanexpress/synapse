92558a7fd8bea44eacb392f9c88e049ee3eba05e
Wendy Hu <wendy.hu@alumni.stonybrook.edu>
2022-11-03 14:50:20 -0400

    fix(): adding update handler

---
+++ function/function-samples/sample-function-book-azure/src/main/java/io/americanexpress/function/book/handler/UpdateBookHandler.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 American Express Travel Related Services Company, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.americanexpress.function.book.handler;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+import io.americanexpress.function.book.model.CreateBookResponse;
+import io.americanexpress.function.book.model.UpdateBookRequest;
+import org.springframework.cloud.function.adapter.azure.FunctionInvoker;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+/**
+ * {@code UpdateBookHandler} is the entry point for the read function.
+ */
+public class UpdateBookHandler extends FunctionInvoker<Message<UpdateBookRequest>, CreateBookResponse> {
+
+    /**
+     * Executes read function upon request.
+     *
+     * @param request the request
+     * @param context the context
+     * @return the http response message
+     */
+    @FunctionName("updateBook")
+    public HttpResponseMessage execute(
+            @HttpTrigger(name = "request", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<UpdateBookRequest> request,
+            ExecutionContext context) {
+        Message<UpdateBookRequest> message = MessageBuilder.withPayload(request.getBody())
+                .copyHeaders(request.getHeaders()).build();
+        return request
+                .createResponseBuilder(HttpStatus.OK)
+                .body(handleRequest(message, context))
+                .build();
+    }
+
+}
