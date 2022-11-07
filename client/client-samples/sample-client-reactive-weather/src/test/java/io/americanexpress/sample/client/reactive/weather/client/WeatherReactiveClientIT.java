1f3ec3feff364f6c3ba3885d4994bc878f14324a
Wendy Hu <wendy.hu@alumni.stonybrook.edu>
2022-11-07 14:32:54 -0500

    fix(): fixing it test

--- client/client-samples/sample-client-reactive-weather/src/test/java/io/americanexpress/sample/client/reactive/weather/client/WeatherReactiveClientIT.java
+++ client/client-samples/sample-client-reactive-weather/src/test/java/io/americanexpress/sample/client/reactive/weather/client/WeatherReactiveClientIT.java
@@ -5,28 +5,29 @@ import io.americanexpress.sample.client.reactive.weather.model.WeatherResponse;
 import io.americanexpress.synapse.framework.test.CommonAssertionMessages;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Mono;
 
 /**
  * {@code WeatherReactiveClientIT} tests the {@link WeatherReactiveClient}.
  */
 @ContextConfiguration(classes = WeatherReactiveClientTestConfig.class)
+@ExtendWith(SpringExtension.class)
 class WeatherReactiveClientIT {
 
     /**
      * Weather client
      */
-    private final WeatherReactiveClient client;
-
-    public WeatherReactiveClientIT(WeatherReactiveClient client) {
-        this.client = client;
-    }
+    @Autowired
+    private WeatherReactiveClient client;
 
     @Test
-    void callMonoService_givenValidRequest_expectedSuccessResponse() throws Exception {
+    void callMonoService_givenValidRequest_expectedSuccessResponse() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
