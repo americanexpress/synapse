22bafae41d87f3d43227528c32a4872f062f1c58
andCastStatic <112961962+andCastStatic@users.noreply.github.com>
2022-11-16 16:06:20 -0500

    Feat/oracle data (#124)
    * feat(oracle-data): initial oracle-data module
    
    * Fixed oracle-data - removed spring.oracle.datasource and left it empty
    
    * feat(oracle-data): Added integration test to oracle-data
    
    * feat(oracle-data): building oracle reactive client)
    
    * feat(): oracle-data cleanup
    
    * fix(): merged branch with develop
    
    * fix(): resolved missing java docs, copyrights, renamed test method, moved sample app to sample package
    
    * chore(): refactored reactive components to their respective reactive packages. Fixed test cases.
    
    * chore(): removed commented dependencies
    
    * fix(): fixed 404 bug
    
    * chore(): removed empty test files
    
    * fix(): removed wildcard import and imported individually
    
    * fix(): fixed wildcard imports and made constructors protected
    
    * fix(): removed ecache dependencies
    
    * feat(reactive-oracle): completed reactive-oracle data and sample service
    
    * feat(sample-services): tested oracle-sample services working on reactive oracle-sample services
    
    * feat(oracle): resolved issues with oracle reactive, addressed PR comments, code clean up
    
    * feat(oracle): merged with developed updated branch
    
    * fix(): oracle modules increased to version 0.3.6
    
    * fix(): added copyright to pom.xml deleted commented file
    
    * feat(oracle): resolved missing javadocs and updated persistence
    
    * fix(): added missing javadocs for service in oracle reactive
    
    * fix(): added missing javadoc added extra space in BookApplication
    
    * fix(): removed wavefront
    
    * fix(): added missing javadoc to field and added extra space to postman project
    
    * fix(): added missing line and added java doc to main method for BookApplication
    
    * fix(): removed third party comment
    
    * fix(): added missing line at the eof
    
    * fix(): alphabetize pom
    
    * fix(): deleted commented properties
    
    * feat(oracle): added and updated missing javadocs
    
    * feat(oracle-date): fixed merge conflicts, issues with merge and resolved errors and bugfix with reactive update and delete
    
    * fix(): added missing generic 4xx messages, deleted spring boot classes for data modules and created readme for oracle
    
    * fix(oracle): javadoc
    
    Co-authored-by: Andrew Castillo <Andrew.A.Castillo1@aexp.com>
    Co-authored-by: Andrew Castillo <--local>
    Co-authored-by: ffgutt <guttff@gmail.com>

---
+++ service/service-samples/sample-service-rest-oracle-book/src/main/java/io/americanexpress/service/book/rest/model/BookRequest.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2020 American Express Travel Related Services Company, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.americanexpress.service.book.rest.model;
+
+import io.americanexpress.synapse.service.rest.model.BaseServiceRequest;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * {@code UpdateBookService} Updates a book resource by request criteria.
+ */
+public class BookRequest implements BaseServiceRequest {
+
+    /**
+     * Default constructor
+     */
+    public BookRequest(){}
+
+    /**
+     * Constructor taking in title as string, author as string, and createdBy as string
+     * @param title
+     * @param author
+     * @param createdBy
+     */
+    public BookRequest(String title, String author, String createdBy) {
+        this.title = title;
+        this.author = author;
+        this.createdBy = createdBy;
+    }
+
+    /**
+     * Constructor taking title and author
+     * @param title
+     * @param author
+     */
+    public BookRequest(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+    /**
+     * title
+     */
+    @NotBlank
+    private String title;
+
+    /**
+     * author
+     */
+    @NotBlank
+    private String author;
+
+    /**
+     * createdBy
+     */
+    private String createdBy;
+
+    /**
+     * Gets the title as string
+     * @return
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Sets the title with provided string.
+     * @param title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Gets author as string.
+     * @return
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * Gets createdBy as string.
+     * @return
+     */
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    /**
+     * Sets createdBy with provided string.
+     * @param createdBy
+     */
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    /**
+     * Sets author with provided string.
+     * @param author
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    /**
+     * Returns a string of the obejct
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "BookRequest{" +
+                ", createdBy='" + createdBy + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
