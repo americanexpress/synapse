47b238681987ce12593d7f0a4ced9bafd40b1491
Wendy Hu <wendy.hu@alumni.stonybrook.edu>
2023-01-20 14:05:18 -0500

    fix(): using application json constant

--- client/synapse-client-test/src/main/java/io/americanexpress/synapse/client/test/client/BaseReactiveRestClientUnitTest.java
+++ client/synapse-client-test/src/main/java/io/americanexpress/synapse/client/test/client/BaseReactiveRestClientUnitTest.java
@@ -22,6 +22,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -54,7 +55,7 @@ public abstract class BaseReactiveRestClientUnitTest<I extends BaseClientRequest
                 {
                     try {
                         return Mono.just(ClientResponse.create(HttpStatus.OK)
-                                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                                 .body(mapper.writeValueAsString(mockDefaultClientResponse())).build());
                     } catch (JsonProcessingException exception) {
                         throw new ApplicationServerException(exception);
