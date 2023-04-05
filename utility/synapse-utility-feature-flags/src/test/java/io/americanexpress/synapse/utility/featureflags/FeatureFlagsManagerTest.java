58842372c984d6aea258e1d5d9ce1b6df73e6346
Wendy Hu <wendy.hu@alumni.stonybrook.edu>
2023-04-05 12:14:25 -0400

    feat(util-feature-flags): adding tests

--- utility/synapse-utility-feature-flags/src/test/java/io/americanexpress/synapse/utility/featureflags/FeatureFlagsManagerTest.java
+++ utility/synapse-utility-feature-flags/src/test/java/io/americanexpress/synapse/utility/featureflags/FeatureFlagsManagerTest.java
@@ -14,14 +14,12 @@
 package io.americanexpress.synapse.utility.featureflags;
 
 import io.americanexpress.synapse.utility.featureflags.config.FeatureFlagsTestConfig;
-import io.americanexpress.synapse.utility.featureflags.model.FeatureFlag;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.io.IOException;
-import java.util.Map;
 
 /**
  * {@code FeatureFlagsManagerTest} tests the {@link FeatureFlagsManager}.
@@ -29,16 +27,16 @@ import java.util.Map;
 @ContextConfiguration(classes = FeatureFlagsTestConfig.class)
 class FeatureFlagsManagerTest {
 
-    private static FeatureFlagsManager featureFlagsManager;
+    private FeatureFlagsManager featureFlagsManager;
 
-    @BeforeAll
-    static void init() throws IOException {
+    @BeforeEach
+    void init() throws IOException {
         featureFlagsManager = new FeatureFlagsManager();
     }
 
     @Test
     void getAll_givenFeatureFlags_expectedMappingOfFeatureFlags() {
-        Map<String, FeatureFlag> map = featureFlagsManager.getAll();
+        var map = featureFlagsManager.getAll();
         Assertions.assertAll(
                 () -> Assertions.assertNotNull(map),
                 () -> Assertions.assertEquals(3, map.size())
@@ -46,8 +44,8 @@ class FeatureFlagsManagerTest {
     }
 
     @Test
-    void getFeatureFlag_givenValidName_expectedFeatureFlag() {
-        FeatureFlag featureFlag = featureFlagsManager.get("feature.a");
+    void get_givenValidName_expectedFeatureFlag() {
+        var featureFlag = featureFlagsManager.get("feature.a");
         Assertions.assertAll(
                 () -> Assertions.assertNotNull(featureFlag),
                 () -> Assertions.assertEquals("true", featureFlag.getValue())
@@ -55,7 +53,21 @@ class FeatureFlagsManagerTest {
     }
 
     @Test
-    void getFeatureFlag_givenInvalidName_expectedNull() {
+    void get_givenInvalidName_expectedNull() {
         Assertions.assertNull(featureFlagsManager.get("feature.d"));
+    }
+
+    @Test
+    void update_givenInvalidName_expectedNull() {
+        Assertions.assertNull(featureFlagsManager.update("feature.d", "false"));
+    }
+
+    @Test
+    void update_givenValidNameAndValue_expectedFeatureFlag() {
+        var updatedFeatureFlag = featureFlagsManager.update("feature.a", "false");
+        Assertions.assertAll(
+                () -> Assertions.assertNotNull(updatedFeatureFlag),
+                () -> Assertions.assertEquals("false", updatedFeatureFlag.getValue())
+        );
     }
 }
