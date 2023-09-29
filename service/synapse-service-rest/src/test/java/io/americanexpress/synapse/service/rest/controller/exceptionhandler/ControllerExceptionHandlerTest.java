6c1b25bbe1a005907c45be97fb8084037b0558c2
johnmartinez <jmartinez5120@gmail.com>
2023-09-29 13:57:56 -0400

    Addressing PR comments

--- service/synapse-service-rest/src/test/java/io/americanexpress/synapse/service/rest/controller/exceptionhandler/ControllerExceptionHandlerTest.java
+++ service/synapse-service-rest/src/test/java/io/americanexpress/synapse/service/rest/controller/exceptionhandler/ControllerExceptionHandlerTest.java
@@ -43,7 +43,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.Mockito.when;
 
