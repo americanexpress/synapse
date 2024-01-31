41de08aa7ae5d5274c0798153eda25018313f5a0
tisla1080 <tanvir.a.islam@aexp.com>
2024-01-31 12:37:37 -0500

    docs: add copyright to script

--- service/service-samples/sample-service-db2-book/startup.sh
+++ service/service-samples/sample-service-db2-book/startup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+# Copyright 2020 American Express Travel Related Services Company, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
+# the License.
+
 # Creates and starts up a docker image for db2
 docker run -itd --name db2 --restart unless-stopped -e DBNAME=testdb -v /insert/path/here:/database -e DB2INST1_PASSWORD=password -e LICENSE=accept -p 50000:50000 --privileged=true ibmcom/db2
 
