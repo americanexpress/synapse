a13eee896433a2810ae3223e63fa4ac1f236c27b
guttff <guttff@gmail.com>
2024-08-07 10:09:36 -0400

    feat(base):updating the controller with the entitycreator

--- api/synapse-api-rest-imperative/src/main/java/io/americanexpress/synapse/api/rest/imperative/controller/BaseUpdateImperativeRestController.java
+++ api/synapse-api-rest-imperative/src/main/java/io/americanexpress/synapse/api/rest/imperative/controller/BaseUpdateImperativeRestController.java
@@ -1,6 +1,5 @@
 package io.americanexpress.synapse.api.rest.imperative.controller;
 
-import io.americanexpress.synapse.api.rest.imperative.controller.helpers.CreateResponseEntityCreator;
 import io.americanexpress.synapse.service.imperative.model.BaseServiceRequest;
 import io.americanexpress.synapse.service.imperative.model.BaseServiceResponse;
 import io.americanexpress.synapse.service.imperative.service.BaseService;
@@ -29,7 +28,7 @@ public class BaseUpdateImperativeRestController<
     public ResponseEntity<O> update(@RequestHeader HttpHeaders headers, @RequestBody I serviceRequest) {
         logger.entry(serviceRequest);
         O serviceResponse = service.execute(serviceRequest);
-        ResponseEntity<O> responseEntity = UpdateResponseEntityCreator.create(serviceResponse);
+        ResponseEntity<O> responseEntity = CreateResponseEntityCreator.create(serviceResponse);
         logger.exit();
         return responseEntity;
     }
