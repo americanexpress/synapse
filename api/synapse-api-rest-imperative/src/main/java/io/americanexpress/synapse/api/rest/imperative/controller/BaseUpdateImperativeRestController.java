8d836c171b98e9c997cb00ea7fbf1a09de6ef57a
ffgutt <francois.f.gutt@aexp.com>
2024-08-07 09:50:03 -0400

    feat(base): adding the update controller

---
+++ api/synapse-api-rest-imperative/src/main/java/io/americanexpress/synapse/api/rest/imperative/controller/BaseUpdateImperativeRestController.java
@@ -0,0 +1,36 @@
+package io.americanexpress.synapse.api.rest.imperative.controller;
+
+import io.americanexpress.synapse.api.rest.imperative.controller.helpers.CreateResponseEntityCreator;
+import io.americanexpress.synapse.service.imperative.model.BaseServiceRequest;
+import io.americanexpress.synapse.service.imperative.model.BaseServiceResponse;
+import io.americanexpress.synapse.service.imperative.service.BaseService;
+/**
+ * {@code BaseUpdateService} class specifies the prototypes for performing business logic.
+ *
+ * @param <I> input request type
+ * @param <O> output response type
+ * @author Francois Gutt
+ */
+public class BaseUpdateImperativeRestController<
+            I extends BaseServiceRequest,
+            O extends BaseServiceResponse,
+            S extends BaseService<I, O>
+        > extends BaseController<S> {
+
+    /**
+     * Update a single resource.
+     *
+     * @param headers containing the HTTP headers from the consumer
+     * @param serviceRequest body from the consumer
+     * @return response to the consumer
+     */
+    @PostMapping
+    @Operation(tags = "Update Operation", summary = "Updates a resource")
+    public ResponseEntity<O> update(@RequestHeader HttpHeaders headers, @RequestBody I serviceRequest) {
+        logger.entry(serviceRequest);
+        O serviceResponse = service.execute(serviceRequest);
+        ResponseEntity<O> responseEntity = UpdateResponseEntityCreator.create(serviceResponse);
+        logger.exit();
+        return responseEntity;
+    }
+}
