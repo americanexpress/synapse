5a60abe3b7c6f180306dd9914491dbb2f41dbaa0
Anthony Gonzalez <adgonz@me.com>
2024-11-19 21:05:36 -0800

    Deprecate old service w/ controller modules

---
+++ service/synapse-service-reactive-rest/src/main/java/io/americanexpress/synapse/service/reactive/rest/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @deprecated As of release 0.4.0, replaced by new API module synapse-api-rest-reactive.
+ */
+@Deprecated(since = "0.4.0", forRemoval = true)
+package io.americanexpress.synapse.service.reactive.rest;
---
+++ service/synapse-service-test/src/main/java/io/americanexpress/synapse/service/test/controller/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @deprecated As of release 0.4.0, to be replaced by new API module.
+ */
+@Deprecated(since = "0.4.0", forRemoval = true)
+package io.americanexpress.synapse.service.test.controller;
