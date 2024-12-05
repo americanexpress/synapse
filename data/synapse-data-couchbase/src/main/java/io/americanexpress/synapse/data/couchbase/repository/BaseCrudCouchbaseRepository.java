c80682ec7719e17342e75ddbddf5597bef06f95d
brenoreis <brenoreis@hotmail.com>
2024-12-05 17:07:00 -0500

    extend crud repository

--- data/synapse-data-couchbase/src/main/java/io/americanexpress/synapse/data/couchbase/repository/BaseCrudCouchbaseRepository.java
+++ data/synapse-data-couchbase/src/main/java/io/americanexpress/synapse/data/couchbase/repository/BaseCrudCouchbaseRepository.java
@@ -1,6 +1,7 @@
 package io.americanexpress.synapse.data.couchbase.repository;
 
 import io.americanexpress.synapse.data.couchbase.entity.BaseCouchbaseEntity;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -8,5 +9,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  * @author Darien Liburd
  */
 @NoRepositoryBean
-public interface BaseCrudCouchbaseRepository<T extends BaseCouchbaseEntity, I> extends PagingAndSortingRepository<T, I> {
+public interface BaseCrudCouchbaseRepository<T extends BaseCouchbaseEntity, I> extends PagingAndSortingRepository<T, I>, CrudRepository<T, I> {
 }
