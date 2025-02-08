60e5dd4d3cf06b6e06e2921db98ad71ca5a1372b
johnmartinez <jmartinez5120@gmail.com>
2025-02-07 23:12:10 -0500

    Changes to the dependencies and also, adjustments for JDBC connections.

--- data/synapse-data-jdbc/src/main/java/io/americanexpress/synapse/data/jdbc/config/SynapseJdbcDataConfig.java
+++ data/synapse-data-jdbc/src/main/java/io/americanexpress/synapse/data/jdbc/config/SynapseJdbcDataConfig.java
@@ -9,12 +9,26 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
+/**
+ * Configuration class for setting up JDBC data source and enabling transaction management.
+ *
+ * @author John Robert Martinez Ponce
+ */
 @Configuration
 @ComponentScan(basePackages = "io.americanexpress.synapse.data.jdbc")
 @EnableTransactionManagement
 public class SynapseJdbcDataConfig {
 
-
+    /**
+     * Creates and configures a {@link DataSource} bean.
+     * <p>
+     * This method uses properties prefixed with 'spring.datasource' from the application's
+     * configuration to build the data source. The {@link ConfigurationProperties} annotation
+     * binds the external properties to the data source builder.
+     * </p>
+     *
+     * @return a configured {@link DataSource} instance
+     */
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
--- data/synapse-data-jdbc/src/main/java/io/americanexpress/synapse/data/jdbc/entity/BaseEntity.java
+++ data/synapse-data-jdbc/src/main/java/io/americanexpress/synapse/data/jdbc/entity/BaseEntity.java
@@ -14,16 +14,14 @@
 package io.americanexpress.synapse.data.jdbc.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Version;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.relational.core.mapping.Column;
 
 import java.time.Instant;
 import java.util.Objects;
@@ -41,42 +39,42 @@ public abstract class BaseEntity {
      * Id
      */
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(value = "ID")
     protected Long id;
 
     /**
      * Created Date Time
      */
     @CreatedDate
-    @Column(name = "created_date_time")
+    @Column(value = "CREATED_DATE_TIME")
     protected Instant createdDateTime;
 
     /**
      * Last Modified Date Time
      */
     @LastModifiedDate
-    @Column(name = "last_modified_date_time")
+    @Column(value = "LAST_MODIFIED_DATE_TIME")
     protected Instant lastModifiedDateTime;
 
     /**
      * Created By
      */
     @CreatedBy
-    @Column(name = "created_by")
+    @Column(value = "CREATED_BY")
     protected String createdBy;
 
     /**
      * Last Modified By
      */
     @LastModifiedBy
-    @Column(name = "last_modified_by")
+    @Column(value = "LAST_MODIFIED_BY")
     protected String lastModifiedBy;
 
     /**
      * Version
      */
     @Version
-    @Column(name = "version")
+    @Column(value = "VERSION")
     protected Long version;
 
     /**
