e23d44bd8aa09788f6d2b35587a5ec4c367c6b39
Sahil Z <sahil.maharjan678@gmail.com>
2025-05-05 16:20:57 -0400

    Test Fix For AnyOf Invalid Scenario  (#363)
    * Test fixed with values set for sample nested object
    
    * Test Method name changed.
    
    ---------
    
    Co-authored-by: ZSahil <smaharjan@nuvalence.io>

--- utility/synapse-utilities-common/src/test/java/io/americanexpress/synapse/utilities/common/validator/AnyOfValidatorTest.java
+++ utility/synapse-utilities-common/src/test/java/io/americanexpress/synapse/utilities/common/validator/AnyOfValidatorTest.java
@@ -72,7 +72,7 @@ class AnyOfValidatorTest {
     }
 
     @Test
-    void isValid_givenInvalidMethod_expectedFalse() {
+    void isValid_givenInvalidField_expectedFalse() {
         var builder = mock(ConstraintValidatorContext.ConstraintViolationBuilder.class);
         when(constraintValidatorContext.buildConstraintViolationWithTemplate(anyString())).thenReturn(builder);
         when(anyOf.fieldNames()).thenReturn(new String[] {"randomTestField"});
@@ -93,12 +93,14 @@ class AnyOfValidatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource({",", "'   ','   '", "test,test", "'',''", ",''"})
+    @CsvSource({",", "'   ','   '", "'',''", ",''"})
     void isInvalid_givenObjectWithInvalidFields_expectedFalse(String value1, String value2) {
         var builder = mock(ConstraintValidatorContext.ConstraintViolationBuilder.class);
         when(constraintValidatorContext.buildConstraintViolationWithTemplate(anyString())).thenReturn(builder);
         when(constraintValidatorContext.getDefaultConstraintMessageTemplate()).thenReturn("At least one of the fields %s must be provided.");
         var sampleNestedObject  = new SampleNestedObject();
+        sampleNestedObject.setSomeText1(value1);
+        sampleNestedObject.setSomeText2(value2);
         assertFalse(anyOfValidator.isValid(sampleNestedObject, constraintValidatorContext));
         verify(constraintValidatorContext).buildConstraintViolationWithTemplate("At least one of the fields [someText1, someText2] must be provided.");
     }
