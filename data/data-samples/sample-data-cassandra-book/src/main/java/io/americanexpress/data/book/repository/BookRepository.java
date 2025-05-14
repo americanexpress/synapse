adb673e51b1651e33d743722d0af684ac6d8f4a9
albeortega@gmail.com <albeortega@gmail.com>
2025-05-14 14:42:46 -0400

    feat(test): Testing PR access

--- data/data-samples/sample-data-cassandra-book/src/main/java/io/americanexpress/data/book/repository/BookRepository.java
+++ data/data-samples/sample-data-cassandra-book/src/main/java/io/americanexpress/data/book/repository/BookRepository.java
@@ -23,6 +23,7 @@ import java.util.UUID;
 
 /**
  * {@code BookRepository} is the dao repository to handle the queries for the books table.
+ * test
  */
 @Repository
 public interface BookRepository extends CassandraRepository<BookEntity, UUID> {
