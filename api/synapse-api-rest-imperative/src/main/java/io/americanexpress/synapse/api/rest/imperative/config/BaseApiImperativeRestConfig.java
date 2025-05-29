2e17dc68a1ed688c0bd877e5a04106c97554696a
brenoreis <brenoreis@hotmail.com>
2025-05-29 14:24:00 -0400

    add json message converter

--- api/synapse-api-rest-imperative/src/main/java/io/americanexpress/synapse/api/rest/imperative/config/BaseApiImperativeRestConfig.java
+++ api/synapse-api-rest-imperative/src/main/java/io/americanexpress/synapse/api/rest/imperative/config/BaseApiImperativeRestConfig.java
@@ -17,11 +17,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.americanexpress.synapse.api.rest.imperative.interceptor.MetricInterceptor;
 import io.americanexpress.synapse.framework.exception.config.ExceptionConfig;
 import io.americanexpress.synapse.utilities.common.config.UtilitiesCommonConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * {@code ServiceRestConfig} class sets common configurations for the service layer.
@@ -62,5 +68,27 @@ public class BaseApiImperativeRestConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor).addPathPatterns("/**");
+    }
+
+    /**
+     * Get the JSON message converter.
+     *
+     * @return the JSON message converter.
+     */
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonMessageConverter() {
+        final MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter(defaultObjectMapper);
+        messageConverter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
+        return messageConverter;
+    }
+
+    /**
+     * Configure the message converters.
+     *
+     * @param converters message converters of the application.
+     */
+    @Override
+    public void configureMessageConverters(final List<HttpMessageConverter<?>> converters) {
+        converters.add(jsonMessageConverter());
     }
 }
